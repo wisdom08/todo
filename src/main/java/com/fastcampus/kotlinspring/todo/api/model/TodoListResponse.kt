ff751434f0a0b4405fa8dda5858ba298c16aa212
wisdom08 <wisdom08@kakao.com>
2023-01-16 03:40:07 +0900

    refactor: 컨트롤러 레이어를 코틀린으로 변환

---
+++ src/main/java/com/fastcampus/kotlinspring/todo/api/model/TodoListResponse.kt
@@ -0,0 +1,19 @@
+package com.fastcampus.kotlinspring.todo.api.model
+
+import com.fastcampus.kotlinspring.todo.domain.Todo
+import com.fasterxml.jackson.annotation.JsonIgnore
+
+data class TodoListResponse (
+    val items: List<TodoResponse>,
+) {
+    val size: Int
+        @JsonIgnore
+        get() = items.size
+
+    fun get(index: Int) = items[index]
+
+    companion object {
+        fun of(todoList: List<Todo>) =
+            TodoListResponse(todoList.map(TodoResponse::of))
+    }
+}
